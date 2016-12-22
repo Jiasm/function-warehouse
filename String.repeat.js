5ed77f1146c8cd494844b3f20640788f17416b5a
jiashunming <jiashunming@outlook.com>
2016-12-22 19:40:45 +0800

    feat(deploy): add String.repeat method

---
+++ String.repeat.js
@@ -0,0 +1,12 @@
+'use strict'
+
+module.exports = (str, len) => {
+  if (len === 0) return ''
+  if (len === 1) return `${str}`
+  if (len === 2) return `${str}${str}`
+
+  // if fill methods not realize
+  // you can use
+  // new Array(len).join(str)
+  return new Array(len).fill(str)
+}
