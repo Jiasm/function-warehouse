432c49c995e69f6b311d38425f824b7ccff86fc2
jiashunming <jiashunming@outlook.com>
2017-01-04 21:22:40 +0800

    feat(object): 将一个数组的object所有的key的value合并

--- object/sum-value-from-object-by-key.js
+++ object/sum-value-from-object-by-key.js
@@ -1,5 +1,8 @@
 'use strict'
 
-module.exports = (item) => {
-
-}
+module.exports = list => list.reduce((old = {}, item) => {
+  Object.keys(item).forEach(key => {
+    old[key] = (old[key] | 0) + (item[key] | 0)
+  })
+  return old
+})
---
+++ object/test/sum-value-from-object.js
@@ -0,0 +1,8 @@
+const merge = require('../sum-value-from-object-by-key.js')
+
+console.log(merge([{
+  a: 1,
+  b: 2
+}, {
+  a: 2
+}]))
